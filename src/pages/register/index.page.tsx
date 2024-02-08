29ba90c3022717155739c72cff0f88fc142727bd
kobe-leonel <leonel.farias@kobe.io>
2024-02-08 10:33:29 -0300

    add redirect to register page and set username value

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
@@ -25,10 +27,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   function handleRegister(data: RegisterFormData) {
     console.log(data)
