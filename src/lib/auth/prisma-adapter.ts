f12ae5d2de8df9dbd8ac55129f4b573dfec2f50c
kobe-leonel <leonel.farias@kobe.io>
2024-02-09 10:53:46 -0300

    finish prisma adapter

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Adapter } from 'next-auth/adapters'
 import { parseCookies, destroyCookie } from 'nookies'
